694d587957e147264cae29dc02bf01fae27781a5
Tamim-hossen <103213985+Tamim-hossen@users.noreply.github.com>
2022-04-10 22:12:34 +0600

    Chapter 4

---
+++ Chapter 4/lesson 1/sandbox.js
@@ -0,0 +1,23 @@
+//Md. Tamim Hossen
+//202-15-14414
+// object literals
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+  };
+  
+  console.log(user);
+  console.log(user.age);
+  
+  user.age = 35;
+  console.log(user.age);
+  
+  console.log(user['name']);
+  user['name'] = 'chun-li';
+  console.log(user['name']);
+  
+  console.log(typeof user);
---
+++ Chapter 4/lesson 3/sandbox.js
@@ -0,0 +1,28 @@
+//Md. Tamim Hossen
+//202-15-14414
+// object literals
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login(){
+      console.log('the user logged in');
+    },
+    logout(){
+      console.log('the user logged out');
+    },
+    logBlogs(){
+      // access the blogs here
+      // console.log(this);
+      console.log('this user has written these blogs:');
+      this.blogs.forEach(blog => {
+        console.log(blog);
+      })
+    }
+  };
+  
+  // console.log(this);
+  user.logBlogs();
